bc6e126c54c6a41954eef8d6aeefb8a80d6e5c10
IT21207754 <86506992+Isuru-rangana@users.noreply.github.com>
2023-05-04 22:34:34 +0530

    Client app created

---
+++ app/src/main/java/com/example/tourismapp/DataClass.kt
@@ -0,0 +1,7 @@
+package com.example.tourismapp
+
+class DataClass (var locationId: String? = null,
+    var locationTitle: String? = null,
+    var locationDesc: String? = null,
+    var locationPriority: String? = null
+)
---
+++ app/src/main/java/com/example/tourismapp/MyAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.tourismapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+
+class MyAdapter(private var userList: List<Userdata>, private val listener: MainActivity) :
+    RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imgId: ShapeableImageView? = itemView.findViewById(R.id.imageView)
+        private val nameList: TextView? = itemView.findViewById(R.id.textView)
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    listener.onClick(position)
+                }
+            }
+        }
+
+        fun bind(user: Userdata) {
+            imgId?.setImageResource(user.img)
+            nameList?.text = user.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_home, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    interface MyClickListener {
+        fun onClick(position: Int)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = userList[position]
+        holder.bind(currentItem)
+    }
+}
